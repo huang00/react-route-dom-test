bc09f0ae0dd477a2cb093caa53dae712e81a73d9
huang00 <huang00@users.noreply.github.com>
2019-02-21 14:43:43 +0800

    Update app.js

--- component/app.js
+++ component/app.js
@@ -1,5 +1,5 @@
 import React,{ Component } from 'react';
-import { Link,Route,Switch } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 export default class App extends Component {
     render() {
         return <div>
